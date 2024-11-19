192c8e5835c5d4cd53867d8a4f70a92a5fa0b855
Sandra Moen <sandramoen01@gmail.com>
2024-11-19 09:35:26 +0100

    rewards refactorization cleanup, wip

--- scripts/ui/choose_a_card.gd
+++ scripts/ui/choose_a_card.gd
@@ -1,11 +1,1 @@
 extends MarginContainer
-
-@onready var playable_card: PlayableCard = %PlayableCard
-@onready var playable_card_2: PlayableCard = %PlayableCard2
-@onready var playable_card_3: PlayableCard = %PlayableCard3
-
-
-#func _ready() -> void:
-	#playable_card.connect(pre)
-	#playable_card_2.connect(pre)
-	#playable_card_3.connect(pre)
